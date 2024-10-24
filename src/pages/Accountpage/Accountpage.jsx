dbd26765961c2fb602c79f5120b72ad1961c2f70
Gisellvanegas <productofficerads@fundacioncapital.org>
2024-10-23 19:01:45 -0500

    arreglo del modal de imagen

--- src/pages/Accountpage/Accountpage.jsx
+++ src/pages/Accountpage/Accountpage.jsx
@@ -9,95 +9,98 @@ import Modalimg from "./Modalimg"
 
 
 const Accountpage = () => {
-   const { user } = UseAuth();
- 
-   // Llamada a todos los Hooks primero
-   const [show, setShow] = useState(false);
-   const handleClose = () => setShow(false);
-   const handleShow = () => setShow(true);
- 
-   const [edit, setedit] = useState(false);
-   const closedit = () => setedit(false);
-   const openedit = () => setedit(true);
- 
-   const [account, setaccount] = useState(false);
-   const close = () => setaccount(false);
-   const open = () => setaccount(true);
- 
-   const [img, setimg] = useState(false);
-   const closeimg = () => setimg(false);
-   const openimg = () => setimg(true);
- 
-   // Renderizado condicional después de los Hooks
-   if (!user) {
-     return <Redirect to="/Homepage" />;
-   }
- 
-   return (
-     <>
-       <Container className="cardporfile">
-         <Row className="mt-4">
-           <Col xs={12} className="text-center">
-             <img
-               src="/img/female_avatar.svg"
-               alt="porfile"
-               className="avatar"
-               onClick={openimg}
-             />
-           </Col>
-           <Col className="profile">
-             <p className="text-center mt-1">
-               <b>Name:</b> {user.name}
-             </p>
-             <p className="text-center mt-1">
-               <b>Mail:</b> {user.mail}
-             </p>
-             <p className="text-center mt-1">
-               <b>Role:</b> {user.rol}
-             </p>
-             <Button variant="warning" className="mt-1" onClick={open}>
-               edit account
-             </Button>
-             <Button variant="link mt-1" onClick={openedit}>
-               change Password
-             </Button>
-             <Button
-               variant="link"
-               className="mt-1 text-danger"
-               onClick={handleShow}
-             >
-               Delete account
-             </Button>
-           </Col>
-         </Row>
-       </Container>
-       <DeleteModal
-         show={show}
-         setShow={setShow}
-         handleClose={handleClose}
-         handleShow={handleShow}
-       />
-       <Modaleditpasword
-         edit={edit}
-         setedit={setedit}
-         openedit={openedit}
-         closedit={closedit}
-       />
-       <Modalaccount
-         account={account}
-         setaccount={setaccount}
-         close={close}
-         open={open}
-         user={user}
-       />
-       <Modalimg
-         img={img}
-         setimg={setimg}
-         closeimg={closeimg}
-         openimg={openimg}
-       />
-     </>
-   );
- };
- 
- export default Accountpage;
+  const { user } = UseAuth();
+
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  const [edit, setedit] = useState(false);
+  const closedit = () => setedit(false);
+  const openedit = () => setedit(true);
+
+  const [account, setaccount] = useState(false);
+  const close = () => setaccount(false);
+  const open = () => setaccount(true);
+
+  const [img, setimg] = useState(false);
+  const closeimg = () => setimg(false);
+  const openimg = () => setimg(true);
+
+
+  const [profileImage, setProfileImage] = useState("/img/female_avatar.svg");
+
+
+  if (!user) {
+    return <Redirect to="/Homepage" />;
+  }
+
+  return (
+    <>
+      <Container className="cardporfile">
+        <Row className="mt-4">
+          <Col xs={12} className="text-center">
+            <img
+              src={profileImage} 
+              alt="profile"
+              className="avatar"
+              onClick={openimg}
+            />
+          </Col>
+          <Col className="profile">
+            <p className="text-center mt-1">
+              <b>Name:</b> {user.name}
+            </p>
+            <p className="text-center mt-1">
+              <b>Mail:</b> {user.mail}
+            </p>
+            <p className="text-center mt-1">
+              <b>Role:</b> {user.rol}
+            </p>
+            <Button variant="warning" className="mt-1" onClick={open}>
+              Edit Account
+            </Button>
+            <Button variant="link" className="mt-1" onClick={openedit}>
+              Change Password
+            </Button>
+            <Button
+              variant="link"
+              className="mt-1 text-danger"
+              onClick={handleShow}
+            >
+              Delete Account
+            </Button>
+          </Col>
+        </Row>
+      </Container>
+      <DeleteModal
+        show={show}
+        setShow={setShow}
+        handleClose={handleClose}
+        handleShow={handleShow}
+      />
+      <Modaleditpasword
+        edit={edit}
+        setedit={setedit}
+        openedit={openedit}
+        closedit={closedit}
+      />
+      <Modalaccount
+        account={account}
+        setaccount={setaccount}
+        close={close}
+        open={open}
+        user={user}
+      />
+      <Modalimg
+        img={img}
+        setimg={setimg}
+        closeimg={closeimg}
+        openimg={openimg}
+        setProfileImage={setProfileImage}
+      />
+    </>
+  );
+};
+
+export default Accountpage;
--- src/pages/Accountpage/Modalimg.jsx
+++ src/pages/Accountpage/Modalimg.jsx
@@ -1,40 +1,65 @@
-import React from 'react';
+import React,{ useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
-import Alert from 'react-bootstrap/Alert'
+
 import UseAuth from '../../auth/UseAuth';
 import { Form } from 'react-bootstrap';
 
-function Modalimg({img, setimg,  closeimg, openimg}) {
-   
-  const {logout}=UseAuth();
-  const handledelete=()=>{
-     logout()
+
+function Modalimg({ img, setimg, closeimg, openimg, setProfileImage }) {
+  const { logout } = UseAuth();
+  
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [previewImage, setPreviewImage] = useState(null);
+
+  const handleImageChange = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      
+      const imageUrl = URL.createObjectURL(file);
+      setSelectedImage(file); 
+      setPreviewImage(imageUrl); 
+    }
+  };
+
+  const handleUpdateImage = () => {
+    if (selectedImage) {
+
+      console.log("Imagen seleccionada:", selectedImage);
+      
+     
+      setProfileImage(previewImage); 
+    }
+    closeimg(); 
   };
 
   return (
     <>
-       <Modal show={img} onHide={ closeimg}>
+      <Modal show={img} onHide={closeimg}>
         <Modal.Header closeButton>
-          <Modal.Title>change photo</Modal.Title>
+          <Modal.Title>Change Photo</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-        <Form.Group controlId="formFile" className="mb-3">
-        <Form.Label >photo change</Form.Label>
-        <Form.Control type="file" />
-      </Form.Group>
-        <h2>previsualizacion</h2>
+          <Form.Group controlId="formFile" className="mb-3">
+            <Form.Label>Photo Change</Form.Label>
+            <Form.Control type="file" onChange={handleImageChange} />
+          </Form.Group>
+          <h2>Previsualización</h2>
+          {previewImage && (
+            <img src={previewImage} alt="Preview" style={{ width: '100%', height: 'auto' }} />
+          )}
         </Modal.Body>
         <Modal.Footer>
-          <Button variant="secondary" onClick={ closeimg}>
-           Cancel
+          <Button variant="secondary" onClick={closeimg}>
+            Cancel
           </Button>
-          <Button variant="primary" onClick={closeimg}>
-           update image
+          <Button variant="primary" onClick={handleUpdateImage}>
+            Update Image
           </Button>
         </Modal.Footer>
       </Modal>
     </>
   );
 }
-export default Modalimg
+
+export default Modalimg;
